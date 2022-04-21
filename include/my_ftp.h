35fb80fd45fcdef57aceac0889836b8d633eabc2
aurelenc <aurele.nicolas@epitech.eu>
2022-04-21 18:06:28 +0200

    [*] Organised code

--- include/my_ftp.h
+++ include/my_ftp.h
@@ -10,8 +10,16 @@
 #define MAX_BUFF_SIZE 255
 #define MAX_CLIENTS 10
 
-typedef struct client_socket_s {
+typedef struct client_sock_s {
     int socket;
     char *rbuf;
     char *wbuf;
-} client_socket_t;
+} client_sock_t;
+
+typedef struct server_s {
+    int socket;
+    fd_set rfd;
+    fd_set wfd;
+    struct sockaddr_in addr;
+    socklen_t len;
+} server_t;
--- src/my_ftp.c
+++ src/my_ftp.c
@@ -15,7 +15,7 @@
 #include <stdio.h>
 #include "my_ftp.h"
 
-void new_client(client_socket_t **clients, int client_socket)
+void new_client(client_sock_t **clients, int client_socket)
 {
     int i = 0;
 
@@ -27,49 +27,52 @@ void new_client(client_socket_t **clients, int client_socket)
     clients[i]->wbuf = calloc(sizeof(char), MAX_BUFF_SIZE + 1);
 }
 
-int my_ftp(int ac, char **av)
+void listen_clients(client_sock_t *clients, fd_set *rfd)
 {
-    fd_set rfd;
-    fd_set wfd;
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    // int client_socket;
-    struct sockaddr_in server_addr;
-    socklen_t socklen;
-    // char *buff = calloc(sizeof(char), 256);
-    client_socket_t *clients = calloc(sizeof(client_socket_t), MAX_CLIENTS + 1);
-
-    if (!server_socket)
-        return 84;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(atoi(av[1]));
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    socklen = sizeof(server_addr);
-    if (bind(server_socket, (struct sockaddr *)&server_addr, socklen) < 0)
-        return 84;
-    if (listen(server_socket, MAX_CLIENTS) < 0)
-        return 84;
-    setbuf(stdout, NULL);
-    while (1) {
-        FD_ZERO(&rfd);
-        FD_ZERO(&wfd);
-        FD_SET(server_socket, &rfd);
-        for (int i = 0; clients[i].socket != 0; i++)
-            FD_SET(clients[i].socket, &rfd);
-
-        select(FD_SETSIZE, &rfd, &wfd, NULL, NULL);
-        if (FD_ISSET(server_socket, &rfd)) {
-            new_client(&clients,
-            accept(server_socket, (struct sockaddr *)&server_addr, &socklen));
-        }
-        for (int i = 0; clients[i].socket != 0; i++) {
-            printf("%d", clients[i].socket);
-            if (FD_ISSET(clients[i].socket, &rfd)) {
-                read(clients[i].socket, clients[i].rbuf, MAX_BUFF_SIZE);
-                if (strncmp(clients[i].rbuf, "a", 1) == 0)
-                    dprintf(clients[i].socket, "b\n");
-            }
+    for (int i = 0; clients[i].socket != 0; i++) {
+        printf("%d", clients[i].socket);
+        if (FD_ISSET(clients[i].socket, rfd)) {
+            read(clients[i].socket, clients[i].rbuf, MAX_BUFF_SIZE);
+            if (strncmp(clients[i].rbuf, "a", 1) == 0)
+                dprintf(clients[i].socket, "b\n");
         }
     }
-    close(server_socket);
+}
+
+void ftp_loop(client_sock_t *clients, server_t *server)
+{
+    FD_ZERO(&server->rfd);
+    FD_ZERO(&server->wfd);
+    FD_SET(server->socket, &server->rfd);
+    for (int i = 0; clients[i].socket != 0; i++)
+        FD_SET(clients[i].socket, &server->rfd);
+    select(FD_SETSIZE, &server->rfd, &server->wfd, NULL, NULL);
+    if (FD_ISSET(server->socket, &server->rfd)) {
+        new_client(&clients, accept(server->socket,
+        (struct sockaddr *)&server->addr, &server->len));
+    }
+    listen_clients(clients, &server->rfd);
+}
+
+int my_ftp(int ac, char **av)
+{
+    server_t server;
+    client_sock_t *clients = calloc(sizeof(client_sock_t), MAX_CLIENTS + 1);
+
+    server.socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (!server.socket)
+        return 84;
+    server.addr.sin_family = AF_INET;
+    server.addr.sin_port = htons(atoi(av[1]));
+    server.addr.sin_addr.s_addr = INADDR_ANY;
+    server.len = sizeof(server.addr);
+    if (bind(server.socket, (struct sockaddr *)&server.addr, server.len) < 0)
+        return 84;
+    if (listen(server.socket, MAX_CLIENTS) < 0)
+        return 84;
+    setbuf(stdout, NULL);
+    while (1)
+        ftp_loop(clients, &server);
+    close(server.socket);
     return 0;
 }
