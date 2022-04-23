baaf65e70e2c3871966479aed487443eda9b0cd5
aurelenc <aurele.nicolas@epitech.eu>
2022-04-24 00:45:12 +0200

    [+] Added STOR command

--- src/commands_data.c
+++ src/commands_data.c
@@ -13,7 +13,6 @@ static void retr_file(client_sock_t *clients, int id, int fd)
     int pid = 0;
     char *buff;
 
-    write_client_buff(clients, id, CODE_150);
     buff = calloc(sizeof(char), MAX_BUFF_SIZE + 1);
     if (!buff) {
         write_client_buff(clients, id, CODE_451);
@@ -21,13 +20,36 @@ static void retr_file(client_sock_t *clients, int id, int fd)
     }
     pid = fork();
     if (pid == 0) {
+        write_client_buff(clients, id, CODE_150);
         while (read(fd, buff, MAX_BUFF_SIZE) > 0) {
             write(clients[id].transfer_socket, buff, MAX_BUFF_SIZE);
         }
         close(clients[id].transfer_socket);
         free(buff);
-    } else
         write_client_buff(clients, id, CODE_226);
+    }
+}
+
+static void stor_file(client_sock_t *clients, int id, int fd)
+{
+    int pid = 0;
+    char *buff;
+
+    buff = calloc(sizeof(char), MAX_BUFF_SIZE + 1);
+    if (!buff) {
+        write_client_buff(clients, id, CODE_451);
+        return;
+    }
+    pid = fork();
+    if (pid == 0) {
+        write_client_buff(clients, id, CODE_150);
+        while (read(clients[id].transfer_socket, buff, MAX_BUFF_SIZE) > 0) {
+            write(fd, buff, MAX_BUFF_SIZE);
+        }
+        close(clients[id].transfer_socket);
+        free(buff);
+        write_client_buff(clients, id, CODE_226);
+    }
 }
 
 void retr_command(client_sock_t *clients, int id, server_t *srv, params_t arg)
@@ -56,6 +78,26 @@ void retr_command(client_sock_t *clients, int id, server_t *srv, params_t arg)
 
 void stor_command(client_sock_t *clients, int id, server_t *srv, params_t arg)
 {
+    int fd = 0;
+    char *path;
+
+    if (arg.nb != 2) {
+        write_client_buff(clients, id, CODE_501);
+        return;
+    }
+    path = get_clean_path(clients[id].path, arg.array[1]);
+    if (!path) {
+        write_client_buff(clients, id, CODE_451);
+        return;
+    }
+    fd = open(path, O_WRONLY | O_CREAT, 0644);
+    if (fd == -1) {
+        write_client_buff(clients, id, CODE_550);
+        return;
+    }
+    stor_file(clients, id, fd);
+    close(fd);
+    free(path);
 }
 
 void list_command(client_sock_t *clients, int id, server_t *srv, params_t arg)
